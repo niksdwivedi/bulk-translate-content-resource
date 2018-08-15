19fdbb2b8d2c4f6090275976be227f0d8867b852
Nikhil Dwivedi <niks.dwivedi@gmail.com>
2018-08-15 20:19:29 +1000

    Updating code of main translator to read resx/xml file and translate.

--- TranslationBatch/translate.py
+++ TranslationBatch/translate.py
@@ -1,11 +1,30 @@
 from googletrans import Translator
-translator = Translator()
+import xml.etree.ElementTree as ET
+
+# Initialize the translator to use only allowed service urls
 translator = Translator(service_urls=[
       'translate.google.com',
       'translate.google.co.in',
+      'translate.google.com.au'
     ])
-#translatedObj = translator.translate("Hello World !!!", 'hi','en')
 
-translations = translator.translate(['The quick brown fox', 'jumps over', 'the lazy dog'], dest='hi')
-for translation in translations:
-    print(translation.origin, ' -> ', translation.text)
+contentTree = ET.parse('content.xml')
+root = contentTree.getroot()
+
+# for data in root.findall('data'):
+#     translatedObj = translator.translate(data.find('value').text,dest='hi',src='en')
+#     print(translatedObj.origin, translatedObj.text)
+
+for data in root.findall('data'):
+    for value in data.iter('value'):
+        translatedObj = translator.translate(value.text,dest='hi',src='en')
+        value.text = translatedObj.text
+        print(value.text, translatedObj.text)
+
+contentTree.write('output.xml')
+
+
+
+# translations = translator.translate(['The quick brown fox', 'jumps over', 'the lazy dog'], dest='hi', src='en')
+# for translation in translations:
+#     print(translation.origin, ' -> ', translation.text)
